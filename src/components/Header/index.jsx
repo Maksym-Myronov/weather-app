1867ca39e0d13bf47a2007ab6e3914428337fcde
Maksym <maksym.myronov2018@gmail.com>
2023-12-21 14:11:59 +0200

    create input and redux-toolkit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,13 @@
+import Data from './Date';
+import Input from './Input';
 //Styles
-import styles from './index.module.scss'
+import styles from './index.module.scss';
 
 const Header = () => {
     return (
         <div className={styles.header}>
-        
+            <Input />
+            <Data />
         </div>
     )
 }
