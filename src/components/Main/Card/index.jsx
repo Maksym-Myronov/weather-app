f527d86dfaf924708ddbcd6655b76276ac810e0e
Maksym <maksym.myronov2018@gmail.com>
2023-12-21 14:44:57 +0200

    add card component for added city

--- src/components/Main/Card/index.jsx
+++ src/components/Main/Card/index.jsx
@@ -1,10 +1,14 @@
+//Images
+import plus from '../../../assets/img/Plus.svg'
 //Styles
 import styles from './index.module.scss'
 
 const Card = () => {
     return (
         <div className={styles.card}>
-        
+            <div className={styles.card__container}>
+                <button className={styles.card__btn}><img src={plus} alt="plus" /></button>
+            </div>
         </div>
     )
 }
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,14 @@
+import Card from "./Card"
+import LocalCard from "./LocalCard"
+
 //Styles
 import styles from './index.module.scss'
 
 const Main = () => {
     return (
         <div className={styles.main}>
-        
+            <Card />
+            <LocalCard />
         </div>
     )
 }
