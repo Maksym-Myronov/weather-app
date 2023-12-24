e5b7eea9ea631b706c06397034a772ce47e77816
Maksym <maksym.myronov2018@gmail.com>
2023-12-24 18:08:25 +0200

    [MP-0007] - added temperature in celsius

--- src/components/Main/LocalCard/index.jsx
+++ src/components/Main/LocalCard/index.jsx
@@ -3,17 +3,34 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getTemperature, selectTemperatureData } from '../../../feathers/ip/temperatureSlice';
 //Images
 import snow from '../../../assets/img/Image.svg'
+import cloud from '../../../assets/img/06_cloudy_color.svg'
+import sun from '../../../assets/img/01_sunny_color.svg'
+import rain from '../../../assets/img/11_heavy_rain_color.svg'
+import drizzle from '../../../assets/img/09_light_rain_color.svg'
+import heavyRain from '../../../assets/img/14_thunderstorm_color (1).svg'
+import partlyCloudy from '../../../assets/img/35_partly_cloudy_daytime_color.svg'
 //Styles
 import styles from './index.module.scss'
 
 const LocalCard = () => {
-    const [error, setError] = useState(null);
     const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
     const [currentTime, setCurrentTime] = useState('');
     const [lat, setLat] = useState(0)
     const [lon, setLon] = useState(0)
+    const [celsiusMax, setCelsiusMax] = useState(0);
+    const [celsiusMin, setCelsiusMin] = useState(0);
     const dispatch = useDispatch()
     const options = useSelector(selectTemperatureData);
+
+    useEffect(() => {
+        if (options.temp && options.temp.main) {
+            const tempMaxCelsius = Math.floor(Number(options.temp.main.temp_max) - 273.15);
+            const tempMinCelsius = Math.floor(Number(options.temp.main.temp_min) - 273.15);
+            setCelsiusMax(tempMaxCelsius);
+            setCelsiusMin(tempMinCelsius);
+        }
+    }, [options.temp]); 
 
     const onInputChange = useCallback(() => {
         const fetchData = async () => {
@@ -56,16 +73,14 @@ const LocalCard = () => {
         return () => clearInterval(intervalId);
     }, [currentTime]); 
 
-    const apiUrl = 'http://ip-api.com/json/';
-
     useEffect(() => {
         const getUserLocationFromAPI = async () => {
+            const apiUrl = 'http://ip-api.com/json/';
             try {
                 const response = await fetch(`${apiUrl}`);
                 if (response.ok) {
                     const newData = await response.json();
                     setData(newData);
-                    console.log(newData);
                 } else if (response.status === 429) {
                     setError('Too many requests. Please try again later.');
                 } else {
@@ -76,21 +91,50 @@ const LocalCard = () => {
             }
         };
         getUserLocationFromAPI();
-    }, [lat, lon]);
+    }, []);
+
+    const renderWeatherImage = (weatherCondition) => {
+        let imageSource;
+        switch (weatherCondition) {
+            case 'Thunderstorm':
+                imageSource = heavyRain;
+                break;
+            case 'Drizzle':
+                imageSource = drizzle;
+                break;
+            case 'Rain':
+                imageSource = rain;
+                break;
+            case 'Snow':
+                imageSource = snow;
+                break;
+            case 'Clear':
+                imageSource = sun;
+                break;
+            case 'Clouds':
+                imageSource = cloud;
+                break;
+            default:
+                imageSource = partlyCloudy; 
+                break;
+        }
+
+        return <img src={imageSource} alt={weatherCondition} className={styles.local__card} />;
+    };
 
     return (
         <div className={styles.local}>
             <div className={styles.local__container}>
-                <h1>{data.city}</h1>
+                <h1>{data ? data.city : "Loading..."}</h1>
                 <p className={styles.local__time}>{currentTime}</p>
             </div>
             <div className={styles.local__images}>
-                <img src={snow} alt="snow" />
+                {renderWeatherImage(options.temp && options.temp.weather && options.temp.weather[0]?.main)}
                 <p>{options.temp && options.temp.weather && options.temp.weather[0]?.main}</p>
             </div>
             <div className={styles.local__temperature}>
                 <p>AQI 70</p>
-                <p>{options.temp.main?.feels_like}° {options.temp.main?.temp_min}°</p>
+                <p> {celsiusMax} ° / {celsiusMin} °</p>
             </div>
         </div>
     )
